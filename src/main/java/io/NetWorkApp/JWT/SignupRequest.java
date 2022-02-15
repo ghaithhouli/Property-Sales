696b45f058fe5f1702d4934d79a8d8a3591f5429
ghaithhouli <gaith.houli1@gmail.com>
2022-02-16 00:58:51 +0200

    Create SignupRequest.java

---
+++ src/main/java/io/NetWorkApp/JWT/SignupRequest.java
@@ -0,0 +1,55 @@
+package io.NetWorkApp.JWT;
+
+
+import java.util.HashSet;
+import java.util.List;
+import javax.validation.constraints.*;
+
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private HashSet<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public HashSet<String> getRole() {
+        return this.role;
+    }
+
+    public void setRole(HashSet<String> role) {
+        this.role = role;
+    }
+}
