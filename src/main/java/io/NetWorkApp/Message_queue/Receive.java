edc4daf802ffc7087104ebe4d2f39fa8ff73a890
ghaithhouli <gaith.houli1@gmail.com>
2022-02-16 00:59:58 +0200

    Create Receive.java

---
+++ src/main/java/io/NetWorkApp/Message_queue/Receive.java
@@ -0,0 +1,20 @@
+package io.NetWorkApp.Message_queue;
+
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receive {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+}
